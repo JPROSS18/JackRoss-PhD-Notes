4b21815de84159c1d769a94cd5331f591468d750
JPROSS18 <j.ross25@imperial.ac.uk>
2026-02-01 23:22:56 +0000

    ANODE
    Working with ANODE's

--- models/DynamicalSystems.py
+++ models/DynamicalSystems.py
@@ -2,14 +2,8 @@
 from torchdiffeq import odeint
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib import cm
-from matplotlib import colors
-from mpl_toolkits import mplot3d
-from numpy import linalg as la
-from scipy import stats
-from scipy import spatial as sp
+
 from scipy import integrate as integ
-import pandas as pd
 import torch
 import torch.nn as nn
 from tqdm import tqdm
